16263c4529e19505a16a6214413344ad28ab0748
Hylson <hvescovi@gmail.com>
2023-08-02 07:22:50 -0300

    rm

--- vue/serena_manager/src/scripts/utils.js
+++
@@ -1,7 +0,0 @@
-export default function ajustarImagens(texto) {
-    var myip = "localhost"; // a interface de admin vai rodar sempre em localhost - suposição inicial
-    var url = 'http://' + myip + ':5000/imagem/';
-    console.log(url);
-    var retorno = texto.replace(/<img src=/gi, "<img src=" + url);
-    return retorno;
-}
