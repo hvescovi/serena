6540363ca30f3d76d0fadaee3cf7170283a3d953
Hylson <hvescovi@gmail.com>
2026-04-21 20:07:47 -0300

    chore:removing unused parts

--- other/react/serena_run_react/serena-next-app/src/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const generateRandomSpaces = (count: number): string => {
-    return "&nbsp; ".repeat(count);
-};
-
-export const getRandomNumber = (max: number): number => {
-    return Math.floor(Math.random() * max);
-};
